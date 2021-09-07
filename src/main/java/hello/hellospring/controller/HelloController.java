9b1afc6f24706f37245ad6b072ed654d81546a72
Jeesoeb <jeesoeb5761@gmail.com>
2021-09-07 15:02:58 +0900

    mvc modle 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -11,5 +13,13 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String heloMvc(@RequestParam(name = "name", required = true) String name, Model model) {
+        // RequestParm 에서 name을 파라미터로 받는다.
+        // 방법은 url/hello-mvc?name='값'이다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
