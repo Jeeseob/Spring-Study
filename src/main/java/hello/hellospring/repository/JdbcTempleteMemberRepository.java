b2cacfc8e01cb0aeadf3dd81b34dd471e08ffaf0
Jeeseob <jeeseob5761@gmail.com>
2021-09-14 19:40:15 +0900

    H2 DB 연동, 테스트케이스 추가

---
+++ src/main/java/hello/hellospring/repository/JdbcTempleteMemberRepository.java
@@ -0,0 +1,69 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTempleteMemberRepository implements MemberRepository{
+
+    private  final JdbcTemplate jdbcTemplate;
+    // Spring이 자동으로 인젝션 해줌
+    @Autowired
+    public JdbcTempleteMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        // 쿼리를 짤 필요없이 쿼리를 만들어줌.
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name",member.getName());
+
+        // 키를 받아서 member에 저장.
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource((parameters)));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result =jdbcTemplate.query("select * from member where id =?", memberPowMapper(),id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result =jdbcTemplate.query("select * from member where name =?", memberPowMapper(),name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberPowMapper());
+    }
+
+    // 객체 생성..?
+    private RowMapper<Member> memberPowMapper() {
+        return (rs, rowNum) -> {
+
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcTempleteMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 @Configuration // java에 직접 bean설정해주는 것.
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +25,6 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTempleteMemberRepository(dataSource);
     }
 }
