acd649b2b3f7e067fc1f2c995176d01e48965779
Jeeseob <jeeseob5761@gmail.com>
2022-01-19 11:52:37 +0900

    Bean lifeCycle

--- spring_basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring_basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -11,8 +11,5 @@ import org.springframework.context.annotation.FilterType;
         // @Configuration도 ComponentScan에서 Scan되기 때문에, AppConfig도 등록되게 된다. 때문에 예제에서는 해당 부분을 제외한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig {
-
-// @Autowired를 활용하면, 자동으로 스프링 빈의 의존관계를 만들어줌
-
+public class AutoAppConfig { // @Autowired를 활용하면, 자동으로 스프링 빈의 의존관계를 만들어줌
 }
---
+++ spring_basic/core/src/test/java/hello/core/lifeCycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package hello.core.lifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest() {
+        // 닫는 메소느(close)를 제공하는 ApplicationContext의 자식 class
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
