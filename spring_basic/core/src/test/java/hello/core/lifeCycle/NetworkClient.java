0a54447e0853432d9ad6a4930f774c4ca5c16d06
Jeeseob <jeeseob5761@gmail.com>
2022-01-19 12:28:02 +0900

    Bean lifeCycle

--- spring_basic/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ spring_basic/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,13 +1,22 @@
 package hello.core.lifeCycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+// InitializingBean(초기화 Bean)
+// DisposableBean( 종료 Bean)
+//public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+//        connect();
+//        call("초기화 연결 메세지");
 
     }
 
@@ -26,5 +35,22 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    // @Override // InitializingBean
+    //public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    // @Override // DisposableBean
+    //public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
