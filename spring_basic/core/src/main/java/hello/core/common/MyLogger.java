6e53e34a9a2a4f562ead76cfd5ad92028bd4e6cd
Jeeseob <jeeseob5761@gmail.com>
2022-05-20 22:09:13 +0900

    Object Provider 적용

---
+++ spring_basic/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,41 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/05/20
+ */
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "][" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        // 실제로는 log.info를 활용하여 로그를 쓰면 될 듯
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
---
+++ spring_basic/core/src/main/java/hello/core/web/LogDemoService.java
@@ -0,0 +1,24 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/05/20
+ */
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    // 해당 함수를 사용할때만, bean을 새로 주입한다.(사람마다 다를때 이렇게 처리하면 된다.)
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + testId);
+    }
+}
