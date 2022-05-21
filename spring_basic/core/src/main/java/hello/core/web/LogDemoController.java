b57aad936b2cd2bb4c0e471f81ee908e59370dbf
Jeeseob <jeeseob5761@gmail.com>
2022-05-21 16:00:15 +0900

    완강

--- spring_basic/core/src/main/java/hello/core/web/LogDemoController.java
+++ spring_basic/core/src/main/java/hello/core/web/LogDemoController.java
@@ -19,8 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
@@ -28,7 +28,7 @@ public class LogDemoController {
         String requestURL = request.getRequestURL().toString();
 
 
-        MyLogger myLogger = myLoggerProvider.getObject();
+        //MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- spring_basic/core/src/main/java/hello/core/web/LogDemoService.java
+++ spring_basic/core/src/main/java/hello/core/web/LogDemoService.java
@@ -15,10 +15,11 @@ import org.springframework.stereotype.Service;
 public class LogDemoService {
 
     // 해당 함수를 사용할때만, bean을 새로 주입한다.(사람마다 다를때 이렇게 처리하면 된다.)
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    // private final ObjectProvider<MyLogger> myLoggerProvider;
 
+    private final MyLogger myLogger;
     public void logic(String testId) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+       // MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
