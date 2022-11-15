19d8a5ff4047fb6ec14d755f6073370ce8ea99a7
Jeeseob <jeeseob5761@gmail.com>
2022-11-16 08:41:42 +0900

    Get: 파라미터를 url에 담아 보내는 방식 구현

---
+++ springMVC_basic/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,40 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/16
+ *
+ * 1. 파리미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+        request.getParameterNames().asIterator()
+                        .forEachRemaining(paramName -> System.out.println(paramName + " = " + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - end");
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");    // 데이터가 중복이 되는 경우 getParameterValues를 사용한다. 이때, getParameter를 사용하면, 첫번째 값을 반환한다.
+        Arrays.stream(usernames).forEach(name -> System.out.println("username = " + name));
+    }
+}
