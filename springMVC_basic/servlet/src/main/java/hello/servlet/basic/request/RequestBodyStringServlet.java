2857955405507af149bffd943feed0fa4e296477
Jeeseob <jeeseob5761@gmail.com>
2022-11-16 13:46:15 +0900

    Get: 같은 key를 가지는 파라미터를 여러개 보내는 기능에 대한 주석 추가

---
+++ springMVC_basic/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,20 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/16
+ */
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
