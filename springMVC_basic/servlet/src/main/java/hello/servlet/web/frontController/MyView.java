9de34f7cad0ef2809d093dc246633caf8079715f
Jeeseob <jeeseob5761@gmail.com>
2022-11-17 16:23:48 +0900

    front controller 패턴 적용 - v2

---
+++ springMVC_basic/servlet/src/main/java/hello/servlet/web/frontController/MyView.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontController;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/17
+ */
+public class MyView {
+    private final String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ springMVC_basic/servlet/src/main/java/hello/servlet/web/frontController/v2/ControllerV2.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.frontController.v2;
+
+import hello.servlet.web.frontController.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/17
+ */
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
