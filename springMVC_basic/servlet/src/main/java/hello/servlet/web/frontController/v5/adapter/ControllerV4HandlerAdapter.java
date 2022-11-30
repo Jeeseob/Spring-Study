2883ee8115c82f9b41d0285caf059af53d1458d1
Jeeseob <jeeseob5761@gmail.com>
2022-11-30 18:53:48 +0900

    front controller 패턴 적용 - v5(v4)

--- springMVC_basic/servlet/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV4HandlerAdapter.java
+++ springMVC_basic/servlet/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV4HandlerAdapter.java
@@ -3,6 +3,7 @@ package hello.servlet.web.frontController.v5.adapter;
 import hello.servlet.web.frontController.ModelView;
 import hello.servlet.web.frontController.MyView;
 import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v4.ControllerV4;
 import hello.servlet.web.frontController.v5.MyHandlerAdapter;
 
 import javax.servlet.ServletException;
@@ -16,18 +17,23 @@ import java.util.Map;
  * @Author : Jeeseob
  * @CreateAt : 2022/11/30
  */
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object hadler) {
-        return (hadler instanceof ControllerV3);   // 해당 객체가 ControllerV3의 instance 인지 확인
+        return (hadler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        ControllerV3 controller = (ControllerV3) handler; // frontController에서 support를 활용하여 검증을 하기 때문에, 단순 변환해도 괜찮음.
+        ControllerV4 controller = (ControllerV4) handler; // frontController에서 support를 활용하여 검증을 하기 때문에, 단순 변환해도 괜찮음.
 
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
 
         return mv;
     }
